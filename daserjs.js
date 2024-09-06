16fba58320f31666cb9dac6c5e5df332ce40c1b0
Nicolás Cabrera <nicoucabrera@gmail.com>
2024-09-06 14:26:42 -0300

    #

--- daserjs.js
+++ daserjs.js
@@ -1,18 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const buttons = document.querySelectorAll('.btn');
-    buttons.forEach(button => {
-        button.style.borderColor = getRandomColor();
-    });
-});
-
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+alert("Para mantenerte siempre actualizado con nuestros trabajos te recomendamos borrar el 'Cache' de tu navegador, muchas gracias por visitarnos. \n Atte: Daser Tattoo")
 
 
 
