47fcdb04a120620009e2643e4d7caaf2cdc4b198
Nicolás Cabrera <nicoucabrera@gmail.com>
2024-09-21 15:38:52 -0300

    nuevas modif y se agregaron categorias

--- daserjs.js
+++ daserjs.js
@@ -1,4 +1,14 @@
+function changeCategory() {
+    // Ocultar todas las secciones de imágenes
+    const sections = document.querySelectorAll('.jobs');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
 
+    // Obtener el valor de la selección y mostrar la sección correspondiente
+    const selectedCategory = document.getElementById('categorySelect').value;
+    document.getElementById(selectedCategory).style.display = 'flex';
+}
 
 
 
